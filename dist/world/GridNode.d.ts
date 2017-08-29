41191a9298311b59a8d903880aa24e360e903adc
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-08-29 17:46:14 +0300

    wip

--- dist/world/GridNode.d.ts
+++ dist/world/GridNode.d.ts
@@ -1,13 +1,6 @@
-import { Vector3D, WorldElement, GridConfiguration, Line2D, Angle, Distance } from "..";
+import { WorldElement, GridConfiguration } from "..";
 export declare class GridNode extends WorldElement {
     gridConfiguration: GridConfiguration;
     data: number[][];
     constructor(gridConfiguration: GridConfiguration);
-    initGridDataIfNeeded(): void;
-    add(addressX: number, addressY: number): void;
-    remove(addressX: number, addressY: number): void;
-    addLine(line: Line2D, endIsObstacle: boolean): void;
-    iterateEndOfLine(line: Line2D, line2: Line2D, endIsObstacle: boolean): void;
-    addLineWithAngle(line: Line2D, errorAngle: Angle, endIsObstacle: boolean): void;
-    addDistance(positionOffset: Vector3D, northAngle: Angle, distance: Distance): void;
 }
