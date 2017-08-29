c332dd3b9406c3e536e894a35cb8745d05a0ccfc
Mihail Akimenko <mihails.akimenko@gmail.com>
2017-08-29 15:24:32 +0300

    minor

--- src/test/grid-utils.test.ts
+++ src/test/grid-utils.test.ts
@@ -26,11 +26,6 @@ describe('Grid Utils', () => {
   });
 
 
-
-
-
-
-
   it('getAddress with offset', function () {
     var config = new common.GridConfiguration(2, 10, 1, new common.Vector3D(-10, -10, 0));
     assert.equal(1, common.GridUtils.getAddressX(new common.Point2D(5, 4), config));
@@ -38,7 +33,6 @@ describe('Grid Utils', () => {
     assert.equal(0, common.GridUtils.getAddressX(new common.Point2D(-5, -4), config));
     assert.equal(0, common.GridUtils.getAddressY(new common.Point2D(-5, -4), config));
   });
-
 
   it('addPoint with offset', function () {
     var config = new common.GridConfiguration(2, 10, 1, new common.Vector3D(-10, -10, 0));
@@ -50,13 +44,8 @@ describe('Grid Utils', () => {
   });
 
 
-
-
-
-
-
-  it("Grid update: addLine", function () {
-    var COORD_PRECISSION = 0.01;
+  function createTestWorld() : common.WorldNode{
+        var COORD_PRECISSION = 0.01;
     var ROTATION_PRECISSION = 0.0000001;
 
     var maxObstacle = 1;
@@ -74,38 +63,30 @@ describe('Grid Utils', () => {
     var distanceNode = new common.DeviceNode(distanceTransformation);
 
     robotNode.devices.push(distanceNode);
+    return worldNode;
+  }
 
-    gridNode.addLine(new common.Line2D(new common.Point2D(0, 0), new common.Point2D(0, 0)), false);
-    assert.equal(gridNode.data[5][5], 0);
+  it("Grid update: addLine", function () {
+    var worldNode = createTestWorld();
+    worldNode.gridNode.addLine(new common.Line2D(new common.Point2D(0, 0), new common.Point2D(0, 0)), false);
+    assert.equal(worldNode.gridNode.data[5][5], 0);
   });
 
 
   it("Grid update: ", function () {
-    var COORD_PRECISSION = 0.01;
-    var ROTATION_PRECISSION = 0.0000001;
 
-    var maxObstacle = 1;
-    var gridConfiguration = new common.GridConfiguration(10, 1, maxObstacle, new common.Vector3D(-5, -5, 0));
-    var gridNode = new common.GridNode(gridConfiguration);
-    var robotTransformation = new common.NodeTransformation3D();
-    robotTransformation.position = new common.Vector3D(3, 2, 0);
-    robotTransformation.rotation = new common.Vector3D(0, 0, common.AngleUtils.degreesToRadians(45));
-    var robotNode = new common.RobotNode("filename", robotTransformation);
-    var worldNode = new common.WorldNode(gridNode, robotNode);
+    var worldNode = createTestWorld();
 
-    var distanceTransformation = new common.NodeTransformation3D();
-    distanceTransformation.position = new common.Vector3D(0, 2, 0);
-    distanceTransformation.rotation = new common.Vector3D(0, 0, common.AngleUtils.degreesToRadians(5));
-    var distanceNode = new common.DeviceNode(distanceTransformation);
-
-    robotNode.devices.push(distanceNode);
+    var gridNode = worldNode.gridNode;
+    var gridConfiguration = worldNode.gridNode.gridConfiguration;    
+    var distanceNode = worldNode.robotNode.devices[0];
 
     var distance = new common.Distance(2, common.AngleUtils.createAngleFromDegrees(0), true);
 
-    common.GridUtils.updateGridDistance(gridNode, robotNode, distanceNode, distance);
+    common.GridUtils.updateGridDistance(gridNode, worldNode.robotNode, distanceNode, distance);
 
     assert.equal(gridNode.data[4][9], gridConfiguration.unknownValue);
-    assert.equal(gridNode.data[5][9], maxObstacle);
+    assert.equal(gridNode.data[5][9], gridConfiguration.maxObstacleCount);
     assert.equal(gridNode.data[6][9], gridConfiguration.unknownValue);
 
     assert.equal(gridNode.data[5][8], gridConfiguration.unknownValue);
